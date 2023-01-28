044c857e6d9892ac6242ee147ba62a02a1e14a8f
Acquiesc <alee38@email.davenport.edu>
2023-01-28 18:26:54 -0500

    Bug fix video display
    Workaround github page hosting mp4 issue via gif format.  Upload gifs & adjust html,css,js relating to video and gif on projects.html

--- js/scrollPortfolioGifs.js
+++ js/scrollPortfolioGifs.js
@@ -1,4 +1,5 @@
-const vids = document.getElementById('video');
+const portVids = document.getElementById('portvideo');
+const lpcVids = document.getElementById('lpcvideo')
 
 let index = 1;
 var fileNum = 4;
@@ -6,17 +7,23 @@ var fileNum = 4;
 
 function nextVid() {
     console.log("Next video called")
-    if(vids) {
-        if(index > fileNum-1) {
-            index = 0;
-        }
-        index++;
-        vids.src = "./images/projects/Portfolio/Portfolio" + (index) + ".mp4";
-        console.log("Video set to index source: " + index);
+    if(index > fileNum-1) {
+        index = 0;
+    }
+    index++;
+
+    if(portVids) {
+        portVids.src = "./images/projects/Portfolio/portfolio" + (index) + ".gif";
+        console.log("Portfolio video set to index source: " + index);
+    }
+
+    if(lpcVids) {
+        lpcVids.src = "./images/projects/lpc/lpc" + (index) + ".gif";
+        console.log("LPC video set to index source: " + index);
     }
 }
 
 
 var autoScroll = window.setInterval(function() {
     nextVid();
-}, 7000);
+}, 6900);
