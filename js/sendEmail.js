6128b8eecb9832b9234fe8b6547c27c52b9aebc6
Acquiesc <alee38@email.davenport.edu>
2023-03-05 16:26:52 -0500

    Skill styling, Email JS
    Fixed email JS integration script
    
    adjusted margins on skills display

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,24 +1,26 @@
 const serviceID = "service_eyo4xn8";
 const templateID = "template_4gif7w6";
 
-function sendEmail(params) {
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", function(event) {
     console.log("Reached send mail function");
+    event.preventDefault();
 
     var tempParams = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         phone: document.getElementById("phone").value,
         subject: document.getElementById("subject").value,
-        msg: document.getElementById("msg").value,
+        msg: document.getElementById("msg").value
     };
 
-    emailjs.send(serviceID, templateID, tempParams, "eYU0IJ5TEK13rkVPq")
+    emailjs.sendForm(serviceID, templateID, contactForm)
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
+        contactForm.reset();
+        setInterval(() => window.location.reload(true), 3000);
      }, function(error) {
         console.log('FAILED...', error);
      });
-
-}
-
-document.getElementById("submitBtn").addEventListener("click", sendEmail);
+}) 
