3a8016a88ff518291f5fad5e818d7a98e4db7733
Acquiesc <alee38@email.davenport.edu>
2023-03-09 18:42:31 -0500

    LPC Project
    Moved up LPC project to increase views from users
    
    Cleaned unused parameters from sendEmail.js

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -7,14 +7,6 @@ contactForm.addEventListener("submit", function(event) {
     console.log("Reached send mail function");
     event.preventDefault();
 
-    var tempParams = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        subject: document.getElementById("subject").value,
-        msg: document.getElementById("msg").value
-    };
-
     emailjs.sendForm(serviceID, templateID, contactForm)
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
